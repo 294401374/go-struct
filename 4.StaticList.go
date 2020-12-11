8aa77f29a84eaa3b6703a4fc94f9002849c86cd6
baotong <294401374@qq.com>
2020-12-11 13:46:34 +0800

    静态链表删除节点方法

--- 4.StaticList.go
+++ 4.StaticList.go
@@ -13,6 +13,7 @@ const StaticMaxSize = 10
 //		数组每个节点都会有一个cursor（游标），充当指针的作用
 //		是用头和尾部节点的cursor分别指向剩余的可用节点和元素的头节点
 type StaticNode struct {
+	pos    int
 	Data   string
 	Cursor int
 }
@@ -34,14 +35,15 @@ func (StList *StaticList) InitList(n int) error {
 	for i := 0; i < StaticMaxSize-1; i++ {
 		// 设置cursor
 		StList.List[i].Cursor = i + 1
+		StList.List[i].pos = i + 1
 	}
 	//设置最后一个元素的cursor
 	StList.List[StaticMaxSize-1].Cursor = 0
 	return nil
 }
 
-func (StList *StaticList) CreateNode() (i int) {
-	i = StList.List[0].Cursor
+func (StList *StaticList) CreateNode() int {
+	i := StList.List[0].Cursor
 	if i != 0 {
 		StList.List[0].Cursor = StList.List[i].Cursor
 	}
@@ -52,33 +54,85 @@ func (StList *StaticList) CreateNode() (i int) {
 // 特点：
 //	1、创建元素，第一个元素的cursor改成新元素的cursor
 // 	2、创建一个元素，获取上个元素的cursor并赋给新增的元素把，把上个元素的cursor改成自己
-func (StList *StaticList) Insert(i int, value string) error {
-	var n, h, p int
-	p = StaticMaxSize-1
-	if i < 0 || i > p {
+func (StList *StaticList) InsertElement(i int, value string) error {
+	var n, p int
+	p = StaticMaxSize - 1
+	if i < 0 || i > StList.ListLength()+1 {
 		return errors.New("i不符合规范")
 	}
 	n = StList.CreateNode()
 	if n != 0 {
 		StList.List[n].Data = value
-		// 获取头节点
-		h = StList.List[p].Cursor
 		// 从头节点开始遍历
-		// 写的有问题
-		for j := 1; j < i-1; j++ {
+		for j := 1; j <= i-1; j++ {
 			// 从第一个节点开始遍历
 			// 获取第i-1个的Cursor
 			p = StList.List[p].Cursor
 		}
 		StList.List[n].Cursor = StList.List[p].Cursor
 		StList.List[p].Cursor = n
+		return nil
 	}
 	return errors.New("发生错误")
+}
+
+// 获取链表长度
+func (StList *StaticList) ListLength() int {
+	p := StaticMaxSize - 1
+	var i int
+	for {
+		if StList.List[p].Cursor == 0 {
+			break
+		}
+		p = StList.List[p].Cursor
+		i++
+	}
+	return i
+}
+
+// 释放链表节点
+func (StList *StaticList) FreeElement(i int) {
+	StList.List[i].Cursor = StList.List[0].Cursor
+	StList.List[0].Cursor = i
+}
+
+// 删除节点
+//
+func (StList *StaticList) DeleteNode(i int) error {
+	if i < 0 || i > StList.ListLength()+1 {
+		return errors.New("i 不规范")
+	}
+	p := StaticMaxSize - 1
+	for j := 0; j < i-1; j ++ {
+		p = StList.List[p].Cursor
+	}
+	StList.List[p].Cursor = StList.List[i].Cursor
+	StList.FreeElement(i)
+	return nil
 }
 
 func main() {
 	SList := &StaticList{}
 	SList.InitList(5)
 	fmt.Println(*SList)
+	SList.InsertElement(1, "A")
+	length := SList.ListLength()
+	fmt.Println(*SList)
+	fmt.Println(length)
+	SList.InsertElement(2, "B")
+	length = SList.ListLength()
+	fmt.Println(*SList)
+	fmt.Println(length)
+	SList.InsertElement(3, "C")
+	SList.InsertElement(4, "D2")
+	SList.InsertElement(4, "D1")
+	SList.InsertElement(6, "D3")
+	fmt.Println(*SList)
+	length = SList.ListLength()
+	fmt.Println("len:",length)
+	SList.DeleteNode(1)
+	length = SList.ListLength()
+	fmt.Println(*SList)
+	fmt.Println("len:", length)
 
 }
